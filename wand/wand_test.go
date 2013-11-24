3292f84d8a3b58b26afa9a4e26c598c45cc87baf
Tony Wilson <thinqa@myopera.com>
2013-11-24 11:46:45 +0200

    test

--- wand/wand_test.go
+++ wand/wand_test.go
@@ -2,6 +2,7 @@ package wand
 
 import (
 	G "github.com/tHinqa/outside-graphicsmagick"
+	"os"
 	"testing"
 )
 
@@ -29,4 +30,28 @@ func TestQuery(t *testing.T) {
 	var n uint32
 	t.Log(n, "fonts", QueryFonts("*", &n))
 	t.Log(n, "formats", QueryFormats("*", &n)) // 0 !?
+}
+
+var seed = []byte{71, 73, 70, 56, 57, 97, 1, 0, 1, 0, 240, 0,
+	0, 255, 255, 255, 0, 0, 0, 33, 249, 4, 0, 0, 0, 0, 0,
+	44, 0, 0, 0, 0, 1, 0, 1, 0, 0, 2, 2, 68, 1, 0, 59}
+
+func TestMagickWand(t *testing.T) {
+	w := NewMagickWand()
+	defer w.Destroy()
+	if w.ReadBlob(&seed[0], uint32(len(seed))) {
+		w.Resize(200, 200, 0, 0)
+		w.AddNoise(G.GaussianNoise)
+		w.Edge(0)
+		w.Enhance()
+		w.Despeckle()
+		w.Emboss(10, 2)
+		w.Equalize()
+		for _, f := range []string{"png" /*, "gif", "jpg", "svg", "xbm"*/} {
+			t.Log(f, w.Write("_test."+f))
+			if e := os.Remove("_test." + f); e != nil {
+				t.Error(e)
+			}
+		}
+	}
 }
